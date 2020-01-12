9f85f09a64f569c7d8a10447846f24b4b35b13da
sadhu <1714222385@qq.com>
2020-01-12 12:03:47 +0800

    补充加上 axios 多样调用方式在 axios.ts 中的注释

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import { AxiosInstance } from './types/index'
 import Axios from './core/Axios'
 import { extend } from './helpers/utill'
 
-function createInstance(): AxiosInstance {
+function createInstance(): AxiosInstance { // 制造出多样的调用方式，混合对象（对象本身是 axios 函数，函数上又有一些属性）
   const context = new Axios()
   const instance = Axios.prototype.request.bind(context)
 
