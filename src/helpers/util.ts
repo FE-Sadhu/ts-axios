cb981b17c53fabe00c14ecf7956a70705af7273f
sadhu <1714222385@qq.com>
2020-02-21 17:18:08 +0800

    test(test/helper): cookie's test

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -9,9 +9,9 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object' // 对于 formData、Blob、ArrayBuffer 等对象 typeof 后都是 object,所以有了下面的判断普通对象
-}
+// export function isObject(val: any): val is Object {
+//   return val !== null && typeof val === 'object' // 对于 formData、Blob、ArrayBuffer 等对象 typeof 后都是 object,所以有了下面的判断普通对象
+// }
 
 export function isPlainObject(val: any): val is Object {
   // 判断是否为普通对象。假如对于 formData 的话，toString 后会是 [object FormData]
